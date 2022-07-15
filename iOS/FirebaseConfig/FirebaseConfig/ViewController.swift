590151c907b843b6a987f22c677523fda1bdb8e0
Muhammad Ariful Islam <muhammad.ariful@bjitgroup.com>
2022-07-15 18:16:53 +0600

    Add Firebase Config project
    Add Firebase Config project

---
+++ iOS/FirebaseConfig/FirebaseConfig/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  FirebaseConfig
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
